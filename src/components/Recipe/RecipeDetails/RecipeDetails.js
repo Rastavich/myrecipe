e007f37a8cf2c7bcf128956ce2ed8761e5300182
Isaac Hauser <isaac.hauser@live.com.au>
2020-12-09 23:02:27 +1000

    Update recipe details display logic for null values

--- src/components/Recipe/RecipeDetails/RecipeDetails.js
+++ src/components/Recipe/RecipeDetails/RecipeDetails.js
@@ -9,47 +9,59 @@ const RecipeDetails = (recipe) => {
   }
 
   const { steps } = details;
-  if (steps == null) {
-    return "This recipe has no steps...edit this recipe to add them";
-  }
+  // if (steps == null) {
+  //   return "This recipe has no steps...edit this recipe to add them";
+  // }
 
   const { ingredients } = details;
-  if (ingredients == null) {
-    return "This recipe has no ingredients...edit this recipe to add them";
-  }
+  // if (ingredients == null) {
+  //   return "This recipe has no ingredients...edit this recipe to add them";
+  // }
 
   const { nutrition } = details;
-  if (nutrition == null) {
-    return "This recipe has no nutritional information...edit this recipe to add them";
-  }
+  // if (nutrition == null) {
+  //   return "This recipe has no nutritional information...edit this recipe to add them";
+  // }
 
   return (
     <DetailsView>
       <h5>{details.recipe_intro}</h5>
-      <h4>Ingredients</h4>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <ListView key={ingredient}>
-            <div>
-              <span>
-                <p>{ingredient}&nbsp;</p>
-              </span>
-            </div>
-          </ListView>
-        ))}
-      </ul>
-      <h4>Steps</h4>
-      <ul>
-        {steps.map((step) => (
-          <ListView key={step}>{step}</ListView>
-        ))}
-      </ul>
-      <h4>Nutrition</h4>
-      <ul>
-        {nutrition.map((nutrients) => (
-          <ListView key={nutrients}>{nutrients}</ListView>
-        ))}
-      </ul>
+      {ingredients != null && (
+        <>
+          <h4>Ingredients</h4>
+          <ul>
+            {ingredients.map((ingredient) => (
+              <ListView key={ingredient}>
+                <div>
+                  <span>
+                    <p>{ingredient}&nbsp;</p>
+                  </span>
+                </div>
+              </ListView>
+            ))}
+          </ul>
+        </>
+      )}
+      {steps != null && (
+        <>
+          <h4>Steps</h4>
+          <ul>
+            {steps.map((step) => (
+              <ListView key={step}>{step}</ListView>
+            ))}
+          </ul>
+        </>
+      )}
+      {nutrition != null && (
+        <>
+          <h4>Nutrition</h4>
+          <ul>
+            {nutrition.map((nutrients) => (
+              <ListView key={nutrients}>{nutrients}</ListView>
+            ))}
+          </ul>
+        </>
+      )}
     </DetailsView>
   );
 };
